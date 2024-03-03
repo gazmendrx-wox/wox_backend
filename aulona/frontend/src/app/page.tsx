590cf0e2009e2fe9dd5dc229719adf3c59c9bdeb
aulonalimani <aulona.limani@student.uni-pr.edu>
2024-03-03 22:39:47 +0100

    feat: added Course component

--- aulona/frontend/src/app/page.tsx
+++ aulona/frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Course from "@/components/Course";
 import Person from "@/components/Person";
 
 const persons = [
@@ -9,11 +10,30 @@ const persons = [
   { id: 5, name: "Clirim", city: "Terrnovc" },
 ];
 
+const courses = [
+  { id: 1, name: "HTML & CSS", length_of_weeks: "3 weeks" },
+  { id: 2, name: "JavaScript", length_of_weeks: "4 weeks" },
+  {
+    id: 3,
+    name: "Docker & ExpressJS & PostgreSQL",
+    length_of_weeks: "2 weeks",
+  },
+  { id: 4, name: "ReactJS + NextJS", length_of_weeks: "3 weeks" },
+];
+
 export default function Home() {
   return (
     <>
       {persons.map((person) => (
         <Person key={person.id} name={person.name} city={person.city} />
+      ))}
+
+      {courses.map((course) => (
+        <Course
+          key={course.id}
+          name={course.name}
+          lengthOfWeeks={course.length_of_weeks}
+        />
       ))}
     </>
   );
