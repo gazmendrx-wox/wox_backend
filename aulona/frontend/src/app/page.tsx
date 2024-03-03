942309605c82c2c5156f2ea4e855fde4197c795f
aulonalimani <aulona.limani@student.uni-pr.edu>
2024-03-03 22:03:56 +0100

    chore: configure frontend

---
+++ aulona/frontend/src/app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Person from "@/components/Person";
+
+const persons = [
+  { id: 1, name: "Gazmend", city: "Prishtina" },
+  { id: 2, name: "Aulona", city: "Prishtina" },
+  { id: 3, name: "Artilon", city: "Prishtina" },
+  { id: 4, name: "Edon", city: "Bujanovc" },
+  { id: 5, name: "Clirim", city: "Terrnovc" },
+];
+
+export default function Home() {
+  return (
+    <>
+      {persons.map((person) => (
+        <Person key={person.id} name={person.name} city={person.city} />
+      ))}
+    </>
+  );
+}
