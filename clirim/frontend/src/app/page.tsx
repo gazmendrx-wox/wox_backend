ccffc12bf0d4d453e48ab7c84c39784dbf99a39f
Clirim Kamberi <kambericlirim2@gmail.com>
2024-03-04 17:52:00 +0100

    work with task courses

--- clirim/frontend/src/app/page.tsx
+++ clirim/frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Courses from "./components/Courses"
 import Person from "./components/Person"
 
 const persons = [
@@ -10,10 +11,22 @@ const persons = [
   {id:5, name: 'Clirim', city: 'Terrnovc'}
 ]
 
+const courses = [
+  {id: 1, name: 'html', length_of_weeks: '1 weak' },
+  {id: 2, name: 'css', length_of_weeks: '2 weak' },
+  {id: 3, name: 'javascipt', length_of_weeks: '4 weak' },
+  {id: 4, name: 'postgresql', length_of_weeks: '2 weak' },
+  {id: 5, name: 'express', length_of_weeks: '2 weak' },
+]
+
 export default function Home() {
   return <>
   {persons.map(i => (
       <Person key={i.id} name={i.name} city={i.city} />
+  ))}
+
+  {courses.map(i => (
+    <Courses key={i.id} name={i.name} length_of_weeks={i.length_of_weeks} />
   ))}
   </>
 }
