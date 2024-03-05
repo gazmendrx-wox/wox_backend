64965b702bb78ff0dd30733ac446e1a14967f9c7
Jurik Latifi <jl53598@ubt-uni.net>
2024-03-05 19:15:10 +0100

    feat: i added use-state-homework-1

---
+++ jurik/frontend/src/components/Vendbanim.tsx
@@ -0,0 +1,18 @@
+/* Informata e Dyte = nje komponent me emrin Vendbanim, i cili shfaq vendbanimit e selektuar
+ * nga Parent component*/
+
+export default function Vendbanim({fieldName, selectedOption, changeOption, options}){
+    return <div>
+    {fieldName}:
+      <select value={selectedOption} onChange={(e) => changeOption(e.target.value)}>
+        <option value="">Select a city</option>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    
+
+    </div>
+}
