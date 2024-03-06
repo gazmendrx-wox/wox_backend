a32b657dd0edab1df964f3ae48970a519af6a672
Valon Hajredini <valon.hajredini2@gmail.com>
2024-03-06 18:54:35 +0100

    fix

--- valonhajredini/frontend/components/TextInput.tsx
+++
@@ -1,5 +0,0 @@
-export default function TextInput({fieldName, text, handleChangeText}){
-    return <div>
-        {fieldName}: <input type="text" value={text} placeholder="shkruani nje tekst" onChange={(e) => handleChangeText(e.target.value)}/>
-    </div>
-}
