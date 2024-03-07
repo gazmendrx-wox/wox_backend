3120d73755afd545ab340a33c47ddecd75b34f06
Valon Hajredini <valon.hajredini2@gmail.com>
2024-03-07 21:16:44 +0100

    add

---
+++ valonhajredini/frontend/pages/component-demo-homework/index.tsx
@@ -0,0 +1,18 @@
+"use client"
+/**
+ * DETYRA:
+ *
+ * Me i shfaqe te gjitha te dhanat prej objektit data ne nje page ne next/react;
+ */
+
+import React from "react";
+import handleData from "@/components/objekti";
+
+
+export default function Home(){
+    return<>
+    </>
+    
+    
+    
+}
