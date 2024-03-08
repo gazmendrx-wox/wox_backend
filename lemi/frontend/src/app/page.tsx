5d449c2f87e33a6f4e97a7e438a40c4f822179fe
muharremdemiri <md200111k@riinvest.net>
2024-03-08 14:22:02 +0100

    comiti

--- lemi/frontend/src/app/page.tsx
+++ lemi/frontend/src/app/page.tsx
@@ -1,21 +1,59 @@
 "use client"
 
-import Person from "@/components/Person"
+import Course from "@/components/Courses"
 
 
 
-const persons = [
-  {id:1, name: 'Gazmend', city: 'Prishtina'},
-  {id:2, name: 'Aulona', city: 'Prishtina'},
-  {id:3, name: 'Artilon', city: 'Prishtina'},
-  {id:4, name: 'Edon', city: 'Bujanovc'},
-  {id:5, name: 'Clirim', city: 'Terrnovc'}
-]
+// const persons = [
+//   {id:1, name: 'Gazmend', city: 'Prishtina'},
+//   {id:2, name: 'Aulona', city: 'Prishtina'},
+//   {id:3, name: 'Artilon', city: 'Prishtina'},
+//   {id:4, name: 'Edon', city: 'Bujanovc'},
+//   {id:5, name: 'Clirim', city: 'Terrnovc'}
+// ]
+
+// export default function Home() {
+//   return <>
+//   {persons.map(i => (
+//       <Person key={i.id} name={i.name} city={i.city} />
+//   ))}
+//   </
+// }
+
+/**
+ * Detyra: 
+ * 
+ * Krijo nje komponent te ri, per Courses, i cili liston nga nje array const courses = [], 
+ * nje object course, ka atributet id, name, length_of_weeks 
+ * 
+ * Me shfaq Course: [name] - has lasted [length_of_weeks]
+ */
+
+
+const course = [
+  { id: 1, name: 'lemi', length_of_weeks: 2 },
+  { id: 2, name: 'noli', length_of_weeks: 3 },
+  { id: 3, name: 'argjendi', length_of_weeks: 4 },
+  { id: 4, name: 'drinor', length_of_weeks: 5 }
+];
+
+// function Courses({ course }) {
+//   return (
+//     <div>
+//       <p>Courses: {course.name} - has lasted {course.length_of_weeks} weeks</p>
+//     </div>
+//   );
+// }
 
 export default function Home() {
-  return <>
-  {persons.map(i => (
-      <Person key={i.id} name={i.name} city={i.city} />
-  ))}
-  </>
+  return (
+    <>
+      {course.map(course => (
+        <div  key={course.id}>
+        <Course  name={course.name} length_of_weeks={course.length_of_weeks} />
+
+        </div>
+      ))}
+    </>
+  );
 }
