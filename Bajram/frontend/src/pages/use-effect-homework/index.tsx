eb8dd0372137b11536dc8c7775a2e7644b129b34
BajramAqifi <Aqifibajram5@gmail.com>
2024-03-11 20:55:30 +0100

    useEffect

---
+++ Bajram/frontend/src/pages/use-effect-homework/index.tsx
@@ -0,0 +1,11 @@
+/**
+ * DETYRA:
+ * 
+ * Nje page e cila shfrytezon useEffect per fetching of data nga https://dummyjson.com/docs
+ * 
+ * endpoint sipas zgjedhjes juaj - shembull, Products, Carts, Recipes etj
+ * 
+ * Kerkesa kryesore e detyres eshte demonstrimi i useEffect, edhe Atomic
+ * design principles per ndarjen e komponentave.
+ * 
+ */
