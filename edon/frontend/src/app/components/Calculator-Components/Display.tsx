aa8cb5f5a0975f8a39ff00ca4df8f94c2dee6633
Edon Rexhepi <edonrexhepi01@hotmail.com>
2024-03-11 19:25:03 +0100

    React Calculator

---
+++ edon/frontend/src/app/components/Calculator-Components/Display.tsx
@@ -0,0 +1,8 @@
+export default function Display ({ input, result })  {
+  return (
+    <div className="display">
+      <input type="text" value={input} readOnly />
+      <div>{result}</div>
+    </div>
+  );
+};
