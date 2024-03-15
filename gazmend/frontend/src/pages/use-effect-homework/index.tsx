86d71c08a1fef0ccb91a3dda59c30ac9402a51c0
devGazmendrx <gazmendrx@gmail.com>
2024-03-15 19:25:08 +0100

    useFetch hook

--- gazmend/frontend/src/pages/use-effect-homework/index.tsx
+++ gazmend/frontend/src/pages/use-effect-homework/index.tsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
+
 /**
  * DETYRA:
  * 
  * Nje page e cila shfrytezon useEffect per fetching of data nga https://dummyjson.com/docs
  * 
- * endpoint sipas zgjedhjes juaj - shembull, Products, Carts, Recipes etj
+ * Carts, Recipes edhe Users;
  * 
  * Kerkesa kryesore e detyres eshte demonstrimi i useEffect, edhe Atomic
  * design principles per ndarjen e komponentave.
  * 
  */
+export default function EffectHomework() {
+    const { data, loading } = useFetch("https://dummyjson.com/products")
+
+    if(loading) {
+        console.log('loading...')
+    }
+
+    if(!loading){
+        console.log('data...', data)
+    }
+      
+
+    return <div>
+        <h1>Epic Homework for UseEffect</h1>
+    </div>
+}
