229f5baa79a2f173ab794da6895c78bdcc0cb4f3
Edon Rexhepi <edonrexhepi01@hotmail.com>
2024-03-15 18:59:39 +0100

    useFetch

---
+++ edon/frontend/src/pages/useEffect-data/Company.tsx
@@ -0,0 +1,13 @@
+import Address from "./Address"
+
+
+export default function Company({dataUsers,address,department}){
+    console.log(dataUsers.company , 1)
+    return <div>
+        <h1><Address dataUsers={address}/>
+        Datadepartment:{dataUsers.Datadepartment}
+        name:{dataUsers.name}
+        title:{dataUsers.title}
+        </h1>
+    </div>
+}
