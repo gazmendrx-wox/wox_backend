c675667b305adbaa49769d2923feb4d84dd77e54
BajramAqifi <Aqifibajram5@gmail.com>
2024-03-18 19:36:38 +0100

    ..

--- Bajram/frontend/src/hooks/useFetch.tsx
+++ Bajram/frontend/src/hooks/useFetch.tsx
@@ -1,32 +1,31 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function useFetch(url){
-    const [data, setData] = useState<null | null >(null);
+export default function useFetch(url) {
+    const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true)
 
-useEffect(() => {
-    const getData = async () => {
-      try {
-        fetch(url, {mode: 'no-cors'})
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data)
-            setData(data)
+    useEffect(() => {
+        const getData = async () => {
+          try {
+            fetch(url)
+              .then((res) => res.json())
+              .then((data) => {
+                setData(data)
+                setLoading(false)
+              });
+          } catch (error) {
+            console.error("Error to load data");
             setLoading(false)
-          });
-      } catch (error) {
-        console.error("Error to load data");
-        setLoading(false)
-      }
-    };
+          }
+        };
+    
+        getData();
 
-    getData();
+        //clean up function
+        return () => {
+            setData(null)
+        }
+      }, [url]);
 
-    return () => {
-        setData(null)
-    }
-  }, [url]);
-
-
-  return {data , loading}
+      return { data, loading }
 }
