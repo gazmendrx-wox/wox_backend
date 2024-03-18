a4c6ac48ec157d6b4860fe425915c265e9b2cca2
DionRexhepii <dionrexhepi2016@outlook.com>
2024-03-18 19:36:08 +0100

    fetch

--- dion/frontend-backup/src/pages/use-effect-homework/index.tsx
+++ dion/frontend-backup/src/pages/use-effect-homework/index.tsx
@@ -1,19 +1,30 @@
-import { useEffect, useState } from "react";
-import useFetch from "../use-effect-data/useFetch";
+import { useState, useEffect } from "react";
+import useFetch from "../../hooks/useFetch";
 
+/**
+ * DETYRA:
+ * 
+ * Nje page e cila shfrytezon useEffect per fetching of data nga https://dummyjson.com/docs
+ * 
+ * Carts, Recipes edhe Users;
+ * 
+ * Kerkesa kryesore e detyres eshte demonstrimi i useEffect, edhe Atomic
+ * design principles per ndarjen e komponentave.
+ * 
+ */
 export default function EffectHomework() {
-    const { data, loading } = useFetch('https://dummyjson.com/products')
+    const { data, loading } = useFetch("http://localhost:3001/reviews")
 
     if(loading) {
-        console.log('loading...', loading)
+        console.log('loading...')
     }
 
-    if(!loading) {
-        console.log('data', data)
+    if(!loading){
+        console.log('data...', data)
     }
+      
 
-
-  return <div>
-        <h1>Epic Homework for UseEFFECT</h1>
-  </div>
+    return <div>
+        <h1>Epic Homework for UseEffect</h1>
+    </div>
 }
