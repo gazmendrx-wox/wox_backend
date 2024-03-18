ccd10ee3d0c56eef46a0eb2a0605192f5cb076e6
Artilon Ruhani <artilonruhani6@gmail.com>
2024-03-18 21:14:45 +0100

    fix

--- Artilon/frontend/src/pages/[id]/index.tsx
+++ Artilon/frontend/src/pages/[id]/index.tsx
@@ -1,5 +1,7 @@
-export default function ReviewsByID(){
-    return <div>
-        
-    </div>
+'use client'
+ 
+
+export default function ReviewById({params}){
+
+    return <h1>Hello I am ID: {params.id}</h1>
 }
