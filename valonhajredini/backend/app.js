2a606674c55f001e2fb0bb5863b72af7de70b641
Valon Hajredini <valon.hajredini2@gmail.com>
2024-03-18 20:47:43 +0100

    add

--- valonhajredini/backend/app.js
+++ valonhajredini/backend/app.js
@@ -307,3 +307,30 @@ app.get("/reviews", async (req, res) => {
   }
 });
 
+app.get("/review/:id", async (req, res) => {
+  const { id } = req.params;
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+
+    // Use a parameterized query to prevent SQL injection
+    const result = await client.query(
+      `SELECT * FROM public.reviews where id='${id}'`
+    );
+
+    // Commit the transaction
+    await client.query("COMMIT");
+
+    //const newUser = result;
+    res.json(result.rows);
+  } catch (error) {
+    // Rollback the transaction in case of an error
+    await client.query("ROLLBACK");
+
+    console.error("Error getting review:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    // Release the client back to the pool
+    client.release();
+  }
+});
