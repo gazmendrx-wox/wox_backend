1b3bac8a315a4b9423b2bae20145b47136e7d166
Edon Rexhepi <edonrexhepi01@hotmail.com>
2024-03-18 19:09:46 +0100

    Added new folder

--- edon/frontend-backup/src/pages/useEffect-data/Company.tsx
+++ edon/frontend-backup/src/pages/useEffect-data/Company.tsx
@@ -5,7 +5,7 @@ export default function Company({dataUsers,address,department}){
     console.log(dataUsers.company , 1)
     return <div>
         <h1><Address dataUsers={address}/>
-        Datadepartment:{dataUsers.Datadepartment}
+        Datadepartment:{department}
         name:{dataUsers.name}
         title:{dataUsers.title}
         </h1>
--- edon/frontend-backup/src/pages/useEffect-data/User.tsx
+++ edon/frontend-backup/src/pages/useEffect-data/User.tsx
@@ -2,7 +2,7 @@ import Address from "./Address";
 import Bank from "./Bank";
 import Company from "./Company";
 
-export default function Recipe({id, firstName, lastname, address, bank, company }){
+export default function User({id, firstName, lastname, address, bank, company }){
     return <p key={id}>
         <div> 
             <p> Emri:{firstName} Mbiemri:{lastname} me vendbanim ne :<Address dataUsers={address}/><Bank dataUsers={bank}/><Company dataUsers={company} address={address}/></p>
