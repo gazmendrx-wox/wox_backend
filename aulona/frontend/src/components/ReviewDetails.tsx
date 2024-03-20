b82423f6b09c9b07f4093eb7b8e273ce0e543b10
AulonaLimani <aulona.limani@student.uni-pr.edu>
2024-03-20 13:32:25 +0100

    added review details page

---
+++ aulona/frontend/src/components/ReviewDetails.tsx
@@ -0,0 +1,10 @@
+const ReviewDetails = ({ reviewDetails }) => {
+  return (
+    <p>
+      This <strong>{reviewDetails.value}</strong> is created at{" "}
+      {new Date(reviewDetails.created_at).toLocaleDateString()}
+    </p>
+  );
+};
+
+export default ReviewDetails;
